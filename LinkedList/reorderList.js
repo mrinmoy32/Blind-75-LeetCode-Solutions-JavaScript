8ed3dc2bbb637ad280ccd7af68f923f05ccf2a75
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2024-09-21 21:40:17 +0530

    reorderList

---
+++ LinkedList/reorderList.js
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
+var reorderList = function (head) {
+
+    if (!head || !head.next) {
+        return
+    }
+
+    // middle of the list
+    let fast = head
+    let slow = head
+
+    while (fast && fast.next) {
+        slow = slow.next
+        fast = fast.next.next
+    }
+
+    //revrse later half
+    let curr = slow
+    let prev = null
+
+    while (curr) {
+        let next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+    }
+
+    //merge 1st and 2nd half alternatively
+    first = head
+    second = prev // prev holds the reversed 2nd half
+
+    while (second.next) {
+        let temp1 = first.next;
+        let temp2 = second.next;
+
+        first.next = second;
+        second.next = temp1;
+
+        first = temp1
+        second = temp2
+
+    }
+
+};
