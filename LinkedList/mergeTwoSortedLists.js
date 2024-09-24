5ddc13d517a4caeb9997b60616cf02011f33b4a7
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2024-09-24 18:04:47 +0530

    mergeTwoLists

---
+++ LinkedList/mergeTwoSortedLists.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+
+    const start = new ListNode(0)
+    let current = start
+
+    while (list1 && list2) {
+        if (list1.val < list2.val) {
+            current.next = list1
+            list1 = list1.next
+        } else {
+            current.next = list2
+            list2 = list2.next
+        }
+        current = current.next
+    }
+
+    if (list1 == null) {
+        current.next = list2
+    } else current.next = list1
+
+    return start.next
+};
